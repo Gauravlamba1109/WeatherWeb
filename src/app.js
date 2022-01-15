9d1863d72afe8859d817f08b88eb031f9041ebe5
Gaurav Lamba <gauravlamba1109@gmail.com>
2022-01-16 02:28:00 +0530

    changed port

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const hbs = require('hbs')
 const utils = require('./utils/geocode')
 
 const app = express()
+const port= process.env.PORT || 3000
 
 //define paths for express config 
 const publicDirectoryPath = path.join(__dirname, '../public')
@@ -92,7 +93,7 @@ app.get('*' , (req,res) => {
     })
 })
 
-app.listen(3000, () => {
-    console.log('server is up on port 3000.')
+app.listen(port, () => {
+    console.log('server is up on port '+ port)
 })
 
