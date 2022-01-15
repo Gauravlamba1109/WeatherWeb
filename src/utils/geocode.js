bf51ef4b12271523fb8a3830ae71467856b4474a
Gaurav Lamba <gauravlamba1109@gmail.com>
2022-01-16 02:49:16 +0530

    added more data

--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -36,9 +36,10 @@ const forecast = (data , callback) =>{
             callback('Unable to find location', undefined)
         } 
         else {
-            callback(undefined , body.current.weather_descriptions[0] + ". It is currently " + body.current.temperature 
-            + ' degrees Celsius out. \nFeels like ' + body.current.feelslike 
-            + ' degrees Celsius out.')
+            callback(undefined , 
+                body.current.weather_descriptions[0] + ". It is currently " + body.current.temperature 
+                + ' degrees Celsius out. \nFeels like ' + body.current.feelslike 
+                + ' degrees Celsius out. The humidity is ' + body.humidity + '%.')
         }
     })
 }
